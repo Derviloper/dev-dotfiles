c895da1bed44faa5c741e6ef3f1aec30bde547a1
Derviloper <derviloper@gmx.de>
2026-05-20 13:14:46 +0200

    Fixed vmware-autofit

--- modules/home-manager/vmware-autofit.nix
+++ modules/home-manager/vmware-autofit.nix
@@ -7,16 +7,37 @@
       After = [ "graphical-session.target" ];
     };
     Service = {
-      ExecStart = lib.getExe (pkgs.writeShellApplication {
-        name = "vmware-autofit";
-        runtimeInputs = [ pkgs.xorg.xrandr ];
-        text = ''
-          while sleep 0.5; do
-            xrandr --output Virtual-1 --preferred
-          done
-        '';
-      });
-      Restart = "on-failure";
+      ExecStart = lib.getExe (
+        pkgs.writeShellApplication {
+          name = "vmware-autofit";
+          runtimeInputs = with pkgs; [
+            xorg.xrandr
+            xorg.xev
+            coreutils
+            gawk
+          ];
+          text = ''
+            apply_if_needed() {
+              local query current preferred
+              query=$(xrandr --query)
+              current=$(echo "$query" | awk '/^Virtual-1/{f=1; next} /^[A-Za-z]/{f=0} f && /\*/ {print $1; exit}')
+              preferred=$(echo "$query" | awk '/^Virtual-1/{f=1; next} /^[A-Za-z]/{f=0} f && /\+/ {print $1; exit}')
+              if [ -n "$preferred" ] && [ "$current" != "$preferred" ]; then
+                xrandr --output Virtual-1 --preferred
+              fi
+            }
+
+            apply_if_needed
+            xev -root -event randr | while IFS= read -r line; do
+              case "$line" in
+                *RRScreenChangeNotify*) apply_if_needed ;;
+              esac
+            done
+          '';
+        }
+      );
+      Restart = "always";
+      RestartSec = "2s";
     };
     Install.WantedBy = [ "graphical-session.target" ];
   };
