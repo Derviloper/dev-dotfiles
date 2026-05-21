4082ec50402f5362d1456ad349d72617884a9cdc
Derviloper <derviloper@gmx.de>
2026-05-21 13:54:17 +0200

    Fixed dotfiles checkout

--- modules/nixos/checkout-dotfiles.nix
+++ modules/nixos/checkout-dotfiles.nix
@@ -1,15 +1,38 @@
 {
   pkgs,
   username,
+  homeDirectory,
   dotfiles,
   ...
 }:
 {
-  system.activationScripts.checkoutDotfiles.text = ''
-    if [ ! -d ${dotfiles}/.git ]; then
-      mkdir -p ${dotfiles}
-      ${pkgs.git}/bin/git clone https://github.com/Derviloper/dev-dotfiles ${dotfiles}
-      chown -R ${username}:users ${dotfiles}
-    fi
-  '';
+  systemd.tmpfiles.rules = [
+    "d ${homeDirectory}/Projects 0755 ${username} users -"
+  ];
+
+  systemd.services.checkout-dotfiles = {
+    description = "Clone dotfiles repository on first boot";
+    wantedBy = [ "multi-user.target" ];
+    after = [
+      "network-online.target"
+      "local-fs.target"
+    ];
+    wants = [ "network-online.target" ];
+    before = [
+      "display-manager.service"
+      "getty@tty1.service"
+    ];
+    unitConfig.ConditionPathExists = "!${dotfiles}/.git";
+    serviceConfig = {
+      Type = "oneshot";
+      User = username;
+      Group = "users";
+      Environment = [
+        "GIT_SSL_CAINFO=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
+        "HOME=${homeDirectory}"
+      ];
+      ExecStartPre = "${pkgs.coreutils}/bin/mkdir -p ${dotfiles}";
+      ExecStart = "${pkgs.git}/bin/git clone https://github.com/Derviloper/dev-dotfiles ${dotfiles}";
+    };
+  };
 }
