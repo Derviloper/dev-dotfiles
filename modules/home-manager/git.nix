bcb70fbcc7bf35e40aaabe46367087156116a7e3
Derviloper <derviloper@gmx.de>
2026-05-26 14:24:17 +0200

    Added git commit signing

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -1,11 +1,20 @@
+{ homeDirectory, ... }:
 {
+  home.file.".ssh/id_ed25519.pub".text = ''
+    ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILBm+ebJElO2PL4BqWgb/wdM+QZPYshQRDTSwnBGYobz
+  '';
+
   programs.git = {
     enable = true;
     settings = {
       user = {
         name = "Derviloper";
         email = "derviloper@gmx.de";
+        signingKey = "${homeDirectory}/.ssh/id_ed25519.pub";
       };
+      gpg.format = "ssh";
+      commit.gpgsign = true;
+      tag.gpgsign = true;
     };
   };
 }
